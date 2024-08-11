941a20380dd57ee7d720ccdb83f0996f534beaff
fiyoooo <fiona.froehler@gmail.com>
2024-08-12 00:55:03 +0200

    Type corrections and minor logic corrections.

--- include/MaxSATDecoder.hpp
+++ include/MaxSATDecoder.hpp
@@ -3,12 +3,10 @@
 //
 #pragma once
 
-#include <unordered_map>
-#include <z3++.h>
-
 #include "Decoder.hpp"
 
-// using idk = std::vector<std::size_t> // TODO
+#include <unordered_map>
+#include <z3++.h>
 
 class MaxSATDecoder : public Decoder {
 public:
@@ -16,30 +14,25 @@ public:
 
     MaxSATDecoder(Code& c);
 
-    void decode(const gf2Vec &syndrome) override;
+    void preconstructZ3Instance();
+
+    void decode(const gf2Vec& syndrome) override;
+
+    static std::vector<unsigned> getSwitches(std::size_t light, const std::unique_ptr<ParityCheckMatrix>& pcm);
 
     void reset() override;
 
-    void preconstructZ3Instance();
-
 private:
-    z3::context ctx_; // manages memory allocation and other resources of z3 instance
-    z3::optimize optimizer_; // optimizes constraints and solves problem
-    z3::expr_vector switch_vars_;
-
-    // TODO vectors or sets?
-    // std::unordered_map<std::size_t, std::vector<std::size_t>> lights_to_switches_;
-    // std::unordered_map<std::size_t, std::vector<std::size_t>> switches_to_lights_;
+    z3::context                                      ctx_;       // manages memory allocation and other resources of z3 instance
+    z3::optimize                                     optimizer_; // optimizes constraints and solves problem
+    z3::expr_vector                                  switch_vars_;
     std::unordered_map<std::size_t, z3::expr_vector> helper_vars_;
 
-    static std::vector<std::size_t> getSwitches(std::size_t light, const std::unique_ptr<ParityCheckMatrix> &pcm);
+    void preconstructParityConstraint(std::size_t light, const std::vector<unsigned>& switches);
 
-    void preconstructParityConstraint(std::size_t light, const std::vector<std::size_t> &switches);
+    void completeParityConstraint(std::size_t light, const std::vector<unsigned>& switches, bool val);
 
-    void completeParityConstraint(std::size_t light, const std::vector<std::size_t> &switches, bool val);
+    bool validateModel(const z3::model& model, const gf2Vec& syndrome);
 
-    // TODO use gf2Vec
-    bool validateModel(const z3::model &model, const std::vector<bool> &lights);
-
-    [[maybe_unused]] std::size_t countSwitches(const z3::model &model);
+    [[maybe_unused]] [[maybe_unused]] std::size_t countSwitches(const z3::model& model);
 };
--- src/MaxSATDecoder.cpp
+++ src/MaxSATDecoder.cpp
@@ -9,7 +9,7 @@
 #include <random>
 
 MaxSATDecoder::MaxSATDecoder(Code& c)
-    : Decoder(c), ctx_(), optimizer_(ctx_), switch_vars_(ctx_) {}
+    : Decoder(c), optimizer_(ctx_), switch_vars_(ctx_) {}
 
 /**
  * @brief Preconstruct the z3 instance for the lights-out problem.
@@ -31,7 +31,7 @@ void MaxSATDecoder::preconstructZ3Instance() {
 
     // create helper variables and add known parity constraints
     for (std::size_t light = 0; light < k; light++) {
-        const std::vector<size_t>& switches = getSwitches(light, pcm); // TODO change to gf2Vec
+        const std::vector<unsigned>& switches = getSwitches(light, pcm); // TODO change to gf2Vec
 
         // check if helper variables already exist for light
         if (helper_vars_.find(light) == helper_vars_.end()) {
@@ -49,8 +49,6 @@ void MaxSATDecoder::preconstructZ3Instance() {
     for (const auto& switchVar : switch_vars_) {
         optimizer_.add_soft(!switchVar, 1); // default weight 1 explicitly needed in cpp
     }
-
-    std::cout << "preconstructed z3\n";
 }
 
 /**
@@ -71,15 +69,13 @@ void MaxSATDecoder::decode(const gf2Vec& syndrome) {
 
     // add the problem specific constraints
     for (std::size_t light = 0; light < k; light++) {
-        const std::vector<size_t>& switches = getSwitches(light, pcm); // TODO change to gf2Vec
+        const std::vector<unsigned>& switches = getSwitches(light, pcm); // TODO change to gf2Vec
         completeParityConstraint(light, switches, syndrome[light]);
     }
-    const auto constructionTimeEnd = std::chrono::high_resolution_clock::now();
-    const auto constructionTime    = static_cast<std::size_t>(std::chrono::duration_cast<std::chrono::milliseconds>(
-                                                                   constructionTimeEnd - constructionTimeBegin)
-                                                                   .count());
-
-    std::vector<std::size_t> switches;
+    const auto                  constructionTimeEnd = std::chrono::high_resolution_clock::now();
+    [[maybe_unused]] const auto constructionTime    = static_cast<std::size_t>(std::chrono::duration_cast<std::chrono::milliseconds>(
+                                                                                    constructionTimeEnd - constructionTimeBegin)
+                                                                                    .count());
 
     // solve the problem
     const auto decodingTimeBegin = std::chrono::high_resolution_clock::now();
@@ -89,23 +85,17 @@ void MaxSATDecoder::decode(const gf2Vec& syndrome) {
                                                                  decodingTimeEnd - decodingTimeBegin)
                                                                  .count());
 
-    if (result != z3::sat) {
-        throw std::logic_error("No solution found");
-    }
-
-    // validate the model
-    auto model = optimizer_.get_model();
-    if (!validateModel(model, syndrome)) {
-        throw std::logic_error("Model is invalid");
-    }
-
     // set the switches
-    for (z3::expr const switchVar : switch_vars_) {
-        z3::expr const evalResult = model.eval(switchVar, true);
+    auto                model = optimizer_.get_model();
+    gf2Vec              switchesBool;
+    std::vector<size_t> switchesIdx;
+    for (unsigned i = 0; i < switch_vars_.size(); i++) {
+        z3::expr const evalResult = model.eval(switch_vars_[i], true);
         if (evalResult.is_true()) {
-            switches.push_back(1);
+            switchesBool.push_back(true);
+            switchesIdx.push_back(i);
         } else if (evalResult.is_false()) {
-            switches.push_back(0);
+            switchesBool.push_back(false);
         } else {
             throw std::logic_error("Expression evaluation result is not boolean");
         }
@@ -115,11 +105,16 @@ void MaxSATDecoder::decode(const gf2Vec& syndrome) {
     optimizer_.pop();
 
     // store in result
-    this->result.estimBoolVector = std::vector<bool>(getCode()->getN());
-    for (auto sw : switches) {
-        this->result.estimBoolVector.at(sw) = true;
+    this->result.estimBoolVector    = std::move(switchesBool);
+    this->result.estimNodeIdxVector = std::move(switchesIdx);
+
+    // check found solution
+    if (result != z3::sat) {
+        throw std::logic_error("No solution found");
     }
-    this->result.estimNodeIdxVector = std::move(switches);
+    if (!validateModel(model, syndrome)) {
+        throw std::logic_error("Model is invalid");
+    }
 }
 
 /**
@@ -128,11 +123,11 @@ void MaxSATDecoder::decode(const gf2Vec& syndrome) {
  * @param light in question
  * @param pcm parity check matrix of the code
  */
-std::vector<std::size_t> MaxSATDecoder::getSwitches(std::size_t light, const std::unique_ptr<ParityCheckMatrix>& pcm) {
-    std::vector<std::size_t> switches;
-    const auto&              vec = pcm->pcm->at(light);
+std::vector<unsigned> MaxSATDecoder::getSwitches(std::size_t light, const std::unique_ptr<ParityCheckMatrix>& pcm) {
+    std::vector<unsigned> switches;
+    const auto&           vec = pcm->pcm->at(light);
 
-    for (std::size_t i = 0; i < vec.size(); ++i) {
+    for (unsigned i = 0; i < vec.size(); ++i) {
         if (vec[i]) {
             switches.push_back(i);
         }
@@ -148,7 +143,7 @@ std::vector<std::size_t> MaxSATDecoder::getSwitches(std::size_t light, const std
  * @param light in question
  * @param switches that toggle the light
  */
-void MaxSATDecoder::preconstructParityConstraint(std::size_t light, const std::vector<std::size_t>& switches) {
+void MaxSATDecoder::preconstructParityConstraint(std::size_t light, const std::vector<unsigned>& switches) {
     // ensure switch variables are initialized
     if (switch_vars_.empty()) {
         throw std::logic_error("Switch variables not initialized");
@@ -158,9 +153,8 @@ void MaxSATDecoder::preconstructParityConstraint(std::size_t light, const std::v
     auto& helperVars = helper_vars_.at(light);
 
     // split into smaller constraints
-    for (std::size_t i = 1; i < switches.size() - 1; ++i) {
+    for (unsigned i = 1; i < switches.size() - 1; ++i) {
         // switch_i XOR h_i == h_{i-1}
-        // TODO define constraint outside the loop for more efficiency? But then less memory efficient
         z3::expr const constraint = (switch_vars_[switches[i]] ^ helperVars[i]) == helperVars[i - 1];
         optimizer_.add(constraint.simplify());
     }
@@ -176,10 +170,10 @@ void MaxSATDecoder::preconstructParityConstraint(std::size_t light, const std::v
  * Adds the constraint that is dependent on the value of the light.
  *
  * @param light in question
- * @param switches that toggle the light // TODO put inside function as it is lights_to_switches(light)?
+ * @param switches that toggle the light
  * @param val of the light in the syndrome
  */
-void MaxSATDecoder::completeParityConstraint(std::size_t light, const std::vector<std::size_t>& switches, bool val) {
+void MaxSATDecoder::completeParityConstraint(std::size_t light, const std::vector<unsigned>& switches, bool val) {
     // ensure switch variables are initialized
     if (switch_vars_.empty()) {
         throw std::logic_error("Switch variables not initialized");
@@ -200,19 +194,21 @@ void MaxSATDecoder::completeParityConstraint(std::size_t light, const std::vecto
  * @param lights booleans representing current state of the lights
  * @return true if all lights are switched off after simulating switch presses, false otherwise
  */
-bool MaxSATDecoder::validateModel(const z3::model& model, const std::vector<bool>& lights) {
+bool MaxSATDecoder::validateModel(const z3::model& model, const gf2Vec& syndrome) {
     // ensure switch variables are initialized
     if (switch_vars_.empty()) {
         throw std::logic_error("Switch variables not initialized");
     }
 
-    std::vector<bool> copy = lights;
-    for (std::size_t i = 0; i < switch_vars_.size(); ++i) {
+    gf2Vec copy(syndrome);
+    for (unsigned i = 0; i < switch_vars_.size(); ++i) {
         // if switch set to true
         if (model.eval(switch_vars_[i], true).is_true()) {
-            // flip all lights that are controlled by this switch
-            for (auto light : this->getCode()->gethZ()->pcm->at(i)) {
-                copy[light] = !copy[light];
+            // flip all lights...
+            for (std::size_t light = 0; light < syndrome.size(); light++) {
+                // ...that are controlled by this switch
+                bool const isFlipped = this->getCode()->gethZ()->pcm->at(light).at(i); // TODO check if correct column or row
+                copy[light]          = copy[light] ^ isFlipped;
             }
         }
     }
@@ -227,7 +223,7 @@ bool MaxSATDecoder::validateModel(const z3::model& model, const std::vector<bool
  * @param model to be analyzed
  * @return number of switches that are set to true
  */
-std::size_t MaxSATDecoder::countSwitches(const z3::model& model) {
+[[maybe_unused]] std::size_t MaxSATDecoder::countSwitches(const z3::model& model) {
     // ensure switch variables are initialized
     if (switch_vars_.empty()) {
         throw std::logic_error("Switch variables not initialized");
@@ -252,30 +248,4 @@ void MaxSATDecoder::reset() {
     this->result = {};
     this->growth = GrowthVariant::AllComponents;
     // TODO reset my attributes?
-}
-
-int main() {
-    // initialize lights and solver
-    std::vector<std::vector<bool>> hz = {
-            {1, 1, 1, 0, 0, 0},
-            {0, 1, 1, 0, 1, 0},
-            {0, 1, 0, 1, 1, 0},
-            {0, 0, 1, 0, 1, 1}};
-    Code          c(hz);
-    MaxSATDecoder solver(c);
-    solver.preconstructZ3Instance();
-
-    // solve problem
-    gf2Vec const syndrome = {false, false, false, false};
-    solver.decode(syndrome);
-
-    // output results
-    std::cout << "Switches: ";
-    for (auto& sw : solver.result.estimNodeIdxVector) {
-        std::cout << sw << " ";
-    }
-    // std::cout << "\nConstruction time: " << constr_time.count() << " seconds\n";
-    std::cout << "Solve time: " << solver.result.decodingTime << " seconds\n";
-
-    return 0;
 }
